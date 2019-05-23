816bc52ee6e13b57a919c3831ed8248e9debdfd3
SwChen <swchen1998@qq.com>
2019-05-23 17:53:39 +0800

    add class 'SearchMovie' to respond the search action in front-end

--- test/RandomTest.java
+++ test/RandomTest.java
@@ -6,12 +6,20 @@ import frontEnd.utils.JsonUtils;
 
 import java.util.concurrent.LinkedBlockingQueue;
 
+class A{
+    public int a;
+}
+
+class B extends A{
+    public int b;
+}
 public class RandomTest {
 
     public static void main(String[] args) throws InterruptedException {
-        MovieSystemDB.DBinit();
-        MovieOptions movieOptions = new MovieOptions();
-        System.out.println(JSON.toJSONString(movieOptions.getOptions()));
+        B b = new B();
+        b.a = 1;
+        b.b = 2;
+        System.out.println(JSON.toJSONString(b));
 
     }
 }
